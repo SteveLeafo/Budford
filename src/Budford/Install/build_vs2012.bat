7e9e82fed394701a1e695f75f6a93b9dd40185b2
SteveLeafo <steve.leafo@gmail.com>
2017-11-03 12:20:18 +1100

    Updated the build number method

--- src/Budford/Install/build_vs2012.bat
+++ src/Budford/Install/build_vs2012.bat
@@ -1,10 +1,13 @@
-for /f "delims=" %%A in ('svnversion .') do set "var=%%A"
-echo %var%
-fart ..\Properties\AssemblyInfo.cs 1.0.0.0  0.1.0.%var%
+@echo off
+set /p Build=<ver.txt
+set /a Build=%Build%+1
+echo %Build%>ver.txt
+
+fart ..\Properties\AssemblyInfo.cs 1.0.0.0  0.1.0.%Build%
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\DEVENV"   ..\..\Budford.sln /Rebuild Release
 svn revert ..\Properties\AssemblyInfo.cs
 "c:\Program Files (x86)\NSIS\Bin\makensis.exe" Budford.nsi
 "c:\Program Files (x86)\NSIS\Bin\makensis.exe" "Budford - Full.nsi"
 
-ren "Budford - Setup.exe" "Budford - Setup - V0.1.%var%.exe" 
-ren "Budford - Full Setup.exe" "Budford - Full Setup - V0.1.%var%.exe" 
+ren "Budford - Setup.exe" "Budford - Setup - V0.1.%Build%.exe" 
+ren "Budford - Full Setup.exe" "Budford - Full Setup - V0.1.%Build%.exe" 
