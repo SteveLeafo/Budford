0cabf8a1b1e3ce4722614c3ad9eb7b4d87657f1c
SteveLeafo <steve.leafo@gmail.com>
2017-11-23 16:22:35 +1100

    Versioning changes

--- src/Budford/Install/build_vs2012.bat
+++ src/Budford/Install/build_vs2012.bat
@@ -3,11 +3,11 @@ set /p Build=<ver.txt
 set /a Build=%Build%+1
 echo %Build%>ver.txt
 
-fart ..\Properties\AssemblyInfo.cs 1.0.0.0  0.1.0.%Build%
+fart ..\Properties\AssemblyInfo.cs 1.0.0.0  1.0.%Build%
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\DEVENV"   ..\..\Budford.sln /Rebuild Release
 svn revert ..\Properties\AssemblyInfo.cs
 "c:\Program Files (x86)\NSIS\Bin\makensis.exe" Budford.nsi
 "c:\Program Files (x86)\NSIS\Bin\makensis.exe" "Budford - Full.nsi"
 
-ren "Budford - Setup.exe" "Budford - Setup - V0.1.%Build%.exe" 
-ren "Budford - Full Setup.exe" "Budford - Full Setup - V0.1.%Build%.exe" 
+ren "Budford - Setup.exe" "Budford - Setup - V1.0.%Build%.exe" 
+ren "Budford - Full Setup.exe" "Budford - Full Setup - V1.0.%Build%.exe" 
