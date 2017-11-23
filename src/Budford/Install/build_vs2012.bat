7caae3d8e13a199516363ef5140e79ddc8e51fb8
SteveLeafo <steve.leafo@gmail.com>
2017-11-23 20:00:10 +1100

    Build script changes

--- src/Budford/Install/build_vs2012.bat
+++ src/Budford/Install/build_vs2012.bat
@@ -5,12 +5,12 @@ echo %Build%>ver.txt
 
 fart ..\Properties\AssemblyInfo.cs 1.0.0.0  1.0.%Build%
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\DEVENV"   ..\..\Budford.sln /Rebuild Release
-git commit -m 'New release' ver.txt
+git commit -m "New release" ver.txt
 git status
 git push origin master
 git checkout ..\..
-"c:\Program Files (x86)\NSIS\Bin\makensis.exe" Budford.nsi
-"c:\Program Files (x86)\NSIS\Bin\makensis.exe" "Budford - Full.nsi"
+call "c:\Program Files (x86)\NSIS\Bin\makensis.exe" Budford.nsi
+call "c:\Program Files (x86)\NSIS\Bin\makensis.exe" "Budford - Full.nsi"
 
 ren "Budford - Setup.exe" "Budford - Setup - V1.0.%Build%.exe" 
 ren "Budford - Full Setup.exe" "Budford - Full Setup - V1.0.%Build%.exe" 
