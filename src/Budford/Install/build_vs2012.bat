fbd51db9daa70005ad08d207e729b386d6e14451
SteveLeafo <steve.leafo@gmail.com>
2017-11-28 05:51:36 +1100

    Build dramas

--- src/Budford/Install/build_vs2012.bat
+++ src/Budford/Install/build_vs2012.bat
@@ -10,7 +10,7 @@ git status
 git push origin master
 git checkout ..\..
 call "c:\Program Files (x86)\NSIS\Bin\makensis.exe" Budford.nsi
-rem call "c:\Program Files (x86)\NSIS\Bin\makensis.exe" "Budford - Full.nsi"
+call "c:\Program Files (x86)\NSIS\Bin\makensis.exe" "Budford - Full.nsi"
 
 ren "Budford - Setup.exe" "Budford - Setup - V1.0.%Build%.exe" 
-rem ren "Budford - Full Setup.exe" "Budford - Full Setup - V1.0.%Build%.exe" 
+ren "Budford - Full Setup.exe" "Budford - Full Setup - V1.0.%Build%.exe" 
