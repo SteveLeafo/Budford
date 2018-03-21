57a7ae33ff92f99be421bb1e35b952df50a93681
SteveLeafo <steve.leafo@gmail.com>
2018-03-21 22:33:46 +1000

    Added some files I forgot to check in.

---
+++ src/Budford/Model/ViewPlugin.cs
@@ -0,0 +1,167 @@
+﻿using Budford.Control;
+using Budford.Properties;
+using Budford.View;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Budford.Model
+{
+    internal class ViewPlugin
+    {
+        // All the plug-ins we found
+        private readonly List<PlugIns.PlugIn> plugIns = new List<PlugIns.PlugIn>();
+
+        // The data model
+        private readonly Model Model;
+
+        // A menu to add plug-ins to
+        private readonly ToolStripMenuItem plugInsToolStripMenuItem;
+
+        // The parent form
+        private readonly Form parent;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ModelIn"></param>
+        /// <param name="parentIn"></param>
+        /// <param name="plugInsToolStripMenuItemIn"></param>
+        internal ViewPlugin(Model ModelIn, Form parentIn, ToolStripMenuItem plugInsToolStripMenuItemIn)
+        {
+            Model = ModelIn;
+            parent = parentIn;
+            plugInsToolStripMenuItem = plugInsToolStripMenuItemIn;
+        }
+
+        /// <summary>
+        /// Load all the imported plug-ins into the menu
+        /// </summary>
+        internal void LoadPlugIns()
+        {
+            if (Directory.Exists(SpecialFolders.PlugInFolder(Model)))
+            {
+                List<ToolStripItem> items = new List<ToolStripItem>();
+
+                string currentType = "";
+
+                SearchForPlugins(items);
+
+                AddPlugInsToMenu(items, currentType);
+            }
+        }
+
+        /// <summary>
+        /// Import a plug-in into the model
+        /// </summary>
+        internal void ImportPlugIn()
+        {
+            // Configure open file dialog box 
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Filter = Resources.FormMainWindow_importBudfordPluginToolStripMenuItem_Click_Budford_Plug_in_Files_____xml_;
+
+                // Show open file dialog box 
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    if (!Directory.Exists(SpecialFolders.PlugInFolder(Model)))
+                    {
+                        Directory.CreateDirectory(SpecialFolders.PlugInFolder(Model));
+                    }
+                    FileManager.SafeCopy(dlg.FileName, Path.Combine(SpecialFolders.PlugInFolder(Model), Path.GetFileName(dlg.FileName)), true);
+                    LoadPlugIns();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add plug-ins to menu
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="currentType"></param>
+        private void AddPlugInsToMenu(List<ToolStripItem> items, string currentType)
+        {
+            // Painful, but we want these added to the top of the list...
+            plugInsToolStripMenuItem.DropDownItems.Clear();
+            var v = (from i in items orderby ((PlugIns.PlugIn)i.Tag).Type select i).ToList();
+            foreach (var item in v)
+            {
+                PlugIns.PlugIn p = (PlugIns.PlugIn)item.Tag;
+                if (p.Type != currentType)
+                {
+                    if (currentType != "")
+                    {
+                        plugInsToolStripMenuItem.DropDownItems.Insert(0, new ToolStripSeparator());
+                    }
+                    currentType = p.Type;
+                }
+                plugInsToolStripMenuItem.DropDownItems.Insert(0, item);
+            }
+        }
+
+        /// <summary>
+        /// Search for installed plug-ins
+        /// </summary>
+        /// <param name="items"></param>
+        private void SearchForPlugins(List<ToolStripItem> items)
+        {
+            foreach (var file in Directory.EnumerateFiles(SpecialFolders.PlugInFolder(Model)))
+            {
+                var extension = Path.GetExtension(file);
+                if (extension != null && extension.ToLower().Contains("xml"))
+                {
+                    plugInsToolStripMenuItem.Visible = true;
+
+                    PlugIns.PlugIn p = Persistence.LoadPlugin(file);
+                    plugIns.Add(p);
+
+
+                    ToolStripMenuItem menuItem = new ToolStripMenuItem
+                    {
+                        Text = p.Name,
+                        Tag = p
+                    };
+                    menuItem.Click += PlugIn_Click;
+                    items.Add(menuItem);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute a plug-in
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlugIn_Click(object sender, EventArgs e)
+        {
+            var toolStripMenuItem = sender as ToolStripMenuItem;
+            if (toolStripMenuItem != null)
+            {
+                PlugIns.PlugIn plugIn = toolStripMenuItem.Tag as PlugIns.PlugIn;
+                if (plugIn != null)
+                {
+                    if (plugIn.Type == "ExternalTool")
+                    {
+                        ProcessStartInfo start = new ProcessStartInfo { FileName = plugIn.FileName };
+                        Process.Start(start);
+                    }
+                    else
+                    {
+                        using (FormExecutePlugIn executor = new FormExecutePlugIn(Model, plugIn))
+                        {
+                            if (executor.ShowDialog(parent) == DialogResult.OK)
+                            {
+                                MessageBox.Show(plugIn.Name + Resources.FormMainWindow_PlugIn_Click__executed_successfully, Resources.FormMainWindow_PlugIn_Click_Success);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Budford/Model/ViewUsers.cs
@@ -0,0 +1,287 @@
+﻿using Budford.Control;
+using Budford.Properties;
+using Budford.Tools;
+using Budford.View;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Budford.Model
+{
+    internal class ViewUsers
+    {
+        // The model
+        readonly Model model;
+
+        // Our parent form
+        readonly FormMainWindow mainForm;
+
+        // Context menu from the parent form
+        readonly ContextMenuStrip contextMenuStrip1;
+
+        // Tool strip menu from the parent form
+        readonly ToolStripMenuItem userToolStripMenuItem;
+
+        // The users picture on the main form
+        readonly PictureBox pictureBox1;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ModelIn"></param>
+        /// <param name="mainFormIn"></param>
+        /// <param name="contextMenuStrip1In"></param>
+        /// <param name="userToolStripMenuItemIn"></param>
+        internal ViewUsers(Model ModelIn, FormMainWindow mainFormIn, ContextMenuStrip contextMenuStrip1In, ToolStripMenuItem userToolStripMenuItemIn, PictureBox pictureBox1In)
+        {
+            model = ModelIn;
+            mainForm = mainFormIn;
+            contextMenuStrip1 = contextMenuStrip1In;
+            userToolStripMenuItem = userToolStripMenuItemIn;
+            pictureBox1 = pictureBox1In;
+
+            SetupCurrentUser();
+            AddUserMenuItems();
+
+        }
+
+        /// <summary>
+        /// Edit the current user
+        /// </summary>
+        internal void EditCurrentUser()
+        {
+            User user = model.Users.FirstOrDefault(u => u.Name == model.CurrentUser);
+            using (FormEditUser editUser = new FormEditUser(user))
+            {
+                editUser.ShowDialog(mainForm);
+            }
+
+            if (user != null && File.Exists(Path.Combine("Users", user.Image)))
+            {
+                using (FileStream stream = new FileStream(Path.Combine("Users", user.Image), FileMode.Open, FileAccess.Read))
+                {
+                    pictureBox1.Image = Image.FromStream(stream);
+                }
+            }
+
+            if (user != null && model.CurrentUser != user.Name)
+            {
+                if (Directory.Exists(Path.Combine("Users", model.CurrentUser)))
+                {
+                    try
+                    {
+                        Directory.Move(Path.Combine("Users", model.CurrentUser), Path.Combine("Users", user.Name));
+                    }
+                    catch (Exception ex)
+                    {
+                        model.Errors.Add(ex.Message);
+                    }
+                }
+                UpdateMenuStrip(user);
+                UpdateContextMenuStrip(user);
+
+                model.CurrentUser = user.Name;
+                mainForm.Text = Resources.fMainWindow_fMainWindow_CEMU_Game_DB______Current_User__ + model.CurrentUser;
+            }
+        }
+
+        /// <summary>
+        /// Add a new user item for each user
+        /// </summary>
+        private void AddUserMenuItems()
+        {
+            List<ToolStripMenuItem> items = new List<ToolStripMenuItem>();
+            foreach (var user in model.Users)
+            {
+                ToolStripMenuItem menuItem = new ToolStripMenuItem
+                {
+                    Text = user.Name,
+                    Tag = user
+                };
+                menuItem.Click += User_Click;
+                items.Insert(0, menuItem);
+
+                ToolStripMenuItem menuItem2 = new ToolStripMenuItem
+                {
+                    Text = user.Name,
+                    Tag = user
+                };
+                menuItem2.Click += User_Click;
+                contextMenuStrip1.Items.Add(menuItem2);
+                if (user.Name == model.CurrentUser)
+                {
+                    menuItem2.Checked = true;
+                    menuItem.Checked = true;
+                }
+            }
+
+            // Painful, but we want these added to the top of the list...
+            foreach (var item in items)
+            {
+                userToolStripMenuItem.DropDownItems.Insert(0, item);
+            }
+        }
+
+        /// <summary>
+        /// We are changing user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal void User_Click(object sender, EventArgs e)
+        {
+            var toolStripMenuItem = sender as ToolStripMenuItem;
+            if (toolStripMenuItem != null)
+            {
+                User user = toolStripMenuItem.Tag as User;
+                if (user != null)
+                {
+                    if (user.Name != model.CurrentUser)
+                    {
+                        DeleteAllLockFiles();
+                        if (File.Exists(Path.Combine("Users", user.Image)))
+                        {
+                            using (FileStream stream = new FileStream(Path.Combine("Users", user.Image), FileMode.Open, FileAccess.Read))
+                            {
+                                pictureBox1.Image = Image.FromStream(stream);
+                            }
+
+                        }
+                        mainForm.Text = Resources.fMainWindow_fMainWindow_CEMU_Game_DB______Current_User__ + user.Name;
+                        model.CurrentUser = user.Name;
+                    }
+                }
+                UpdateMenuStrip(user);
+                UpdateContextMenuStrip(user);
+            }
+        }
+
+        /// <summary>
+        /// The user context menu
+        /// </summary>
+        /// <param name="user"></param>
+        internal void UpdateContextMenuStrip(User user)
+        {
+            foreach (ToolStripItem menu in contextMenuStrip1.Items)
+            {
+                var item = menu as ToolStripMenuItem;
+                if (item != null)
+                {
+                    item.Checked = false;
+                }
+                if (menu.Text == model.CurrentUser)
+                {
+                    ((ToolStripMenuItem)menu).Checked = true;
+                    if (user != null)
+                    {
+                        menu.Text = user.Name;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update the menu strip
+        /// </summary>
+        /// <param name="user"></param>
+        internal void UpdateMenuStrip(User user)
+        {
+            foreach (ToolStripItem menu in userToolStripMenuItem.DropDownItems)
+            {
+                var item = menu as ToolStripMenuItem;
+                if (item != null)
+                {
+                    item.Checked = false;
+                }
+                if (menu.Text == model.CurrentUser)
+                {
+                    ((ToolStripMenuItem)menu).Checked = true;
+                    if (user != null)
+                    {
+                        menu.Text = user.Name;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adding a new user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal void addNewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            User newUser = new User() { Name = "User " + (model.Users.Count + 1), Image = "default.png" };
+            using (FormEditUser editUser = new FormEditUser(newUser))
+            {
+                if (editUser.ShowDialog(mainForm) == DialogResult.OK)
+                {
+                    model.Users.Add(newUser);
+                    ToolStripMenuItem menuItem = new ToolStripMenuItem
+                    {
+                        Text = newUser.Name,
+                        Tag = newUser
+                    };
+                    menuItem.Click += User_Click;
+                    userToolStripMenuItem.DropDownItems.Insert(0, menuItem);
+
+                    ToolStripMenuItem menuItem2 = new ToolStripMenuItem
+                    {
+                        Text = newUser.Name,
+                        Tag = newUser
+                    };
+                    menuItem2.Click += User_Click;
+                    contextMenuStrip1.Items.Add(menuItem2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Delete all the lock files
+        /// </summary>
+        private void DeleteAllLockFiles()
+        {
+            CopySaves cs = new CopySaves(model);
+            cs.Execute();
+            foreach (var game in model.GameData.OrderByDescending(gd => gd.Value.Name))
+            {
+                foreach (var version in model.Settings.InstalledVersions)
+                {
+                    DirectoryInfo dest = new DirectoryInfo(SpecialFolders.CurrentUserSaveDirCemu(version, game.Value));
+
+                    string lockFileName = Path.Combine(dest.FullName, "Budford.lck");
+                    FileManager.SafeDelete(lockFileName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set up a user
+        /// </summary>
+        private void SetupCurrentUser()
+        {
+            if (model.Users.Count == 0)
+            {
+                model.Users.Add(new User() { Name = "Default", Image = "default.png" });
+                model.CurrentUser = "Default";
+            }
+
+            var firstOrDefault = model.Users.FirstOrDefault(u => u.Name == model.CurrentUser);
+            if (firstOrDefault != null && File.Exists(Path.Combine("Users", firstOrDefault.Image)))
+            {
+                var orDefault = model.Users.FirstOrDefault(u => u.Name == model.CurrentUser);
+                if (orDefault != null)
+                {
+                    using (FileStream stream = new FileStream(Path.Combine("Users", orDefault.Image), FileMode.Open, FileAccess.Read))
+                    {
+                        pictureBox1.Image = Image.FromStream(stream);
+                    }
+                }
+            }
+        }
+    }
+}
