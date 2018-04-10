aa8f5a2ae3080c2b395140a993bcf0143b0132c0
SteveLeafo <steve.leafo@gmail.com>
2018-04-11 06:01:19 +1000

    Build script now creates zip

--- src/Budford/Install/build_vs2012.bat
+++ src/Budford/Install/build_vs2012.bat
@@ -8,6 +8,11 @@ fart ..\Properties\AssemblyInfo.cs 1.0.0.0  1.0.%Build%
 call "c:\Program Files (x86)\NSIS\Bin\makensis.exe" Budford.nsi
 ren "Budford - Setup.exe" "Budford - Setup - V1.0.%Build%.exe" 
 
+"C:\Program Files\7-Zip\7z.exe" a "Budford - V1.0.%Build%.zip" Users 
+"C:\Program Files\7-Zip\7z.exe" a "Budford - V1.0.%Build%.zip" Budford.exe
+"C:\Program Files\7-Zip\7z.exe" a "Budford - V1.0.%Build%.zip" CNUSLib.dll
+"C:\Program Files\7-Zip\7z.exe" a "Budford - V1.0.%Build%.zip" discord-rpc.dll
+
 set /a Build=%Build%+1
 echo %Build% > ver.txt
 
